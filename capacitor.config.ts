1944e8aa036acbff5bbf6fbf5e47b2052ab69a3b
Fatma Çelik <cutofatma42@gmail.com>
2026-09-02 14:57:40 +0300

    API klasoru aktiflestirildi ve Render icin hazirlandi

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fatmacelik.aliver',
+  appName: 'Aliver',
+  webDir: 'out'
+};
+
+export default config;
